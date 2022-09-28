eaac3be7a5f1e3f63ccabc66fa3a19914183f401
PansonPanson <PansonPanson@163.com>
2022-09-28 11:49:28 +0800

    客户端与服务端双向通信

---
+++ laurel-core/src/main/java/top/panson/laurel/core/core/flash/server/FirstServerHandler.java
@@ -0,0 +1,36 @@
+package top.panson.laurel.core.core.flash.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.Charset;
+import java.util.Date;
+
+/**
+ * @author panson
+ */
+public class FirstServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf byteBuf = (ByteBuf) msg;
+
+        System.out.println(new Date() + ": 服务端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
+
+        // 回复数据到客户端
+        System.out.println(new Date() + ": 服务端写出数据");
+        ByteBuf out = getByteBuf(ctx);
+        ctx.channel().writeAndFlush(out);
+    }
+
+    private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
+        byte[] bytes = "你好客户端，我是服务端!".getBytes(Charset.forName("utf-8"));
+
+        ByteBuf buffer = ctx.alloc().buffer();
+
+        buffer.writeBytes(bytes);
+
+        return buffer;
+    }
+}
